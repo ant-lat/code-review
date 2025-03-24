fd5444162cf0887b1ca86f59aa8dad885ad83ddf
ant-lat <129599502+ant-lat@users.noreply.github.com>
2025-03-24 22:08:59 +0800

    1.0.0
    删除打包参与内容

--- build-package/code-review-api-package/app/routers/code_analysis/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-代码分析路由包
-@author: pgao
-@date: 2024-03-13
-"""
-# 包含代码结构解析、质量检查、代码审查等API路由
-from app.routers.code_analysis.code_analysis import router as code_analysis_router
-from app.routers.code_analysis.code_review import router as code_review_router
